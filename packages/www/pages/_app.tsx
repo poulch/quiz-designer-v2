d39007c2589ef7a86bf6c4cd3e9afd59eda33fcf
Pawel Chyla <chyla1988@gmail.com>
2020-11-05 10:41:52 +0100

    Add antd

--- packages/www/pages/_app.tsx
+++ packages/www/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import 'antd/dist/antd.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
