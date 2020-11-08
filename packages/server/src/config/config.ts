d702c65639453d13d011a70885e040ab242aa2ce
Pawel Chyla <chyla1988@gmail.com>
2020-11-08 17:31:26 +0100

    Mongo db setup

--- packages/server/src/config/config.ts
+++ packages/server/src/config/config.ts
@@ -4,6 +4,8 @@ dotenv();
 
 const config: { [key: string]: any } = {};
 
+config.env = getenv('NODE_ENV', 'development');
 config.port = getenv.int('PORT', 4004);
+config.dbConnectionUrl = getenv('DB_CONNECT_URL');
 
 export default config;
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,40 @@
-import express from 'express';
-import authRouter from './routers/auth';
-import config from './config';
+import app from './app';
+import config from './config/config';
+import mongoose from 'mongoose';
+import logger from './config/logger';
 
-const app = express();
-const port = config.port;
+const PORT = config.port;
+let server: any;
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.connect(config.dbConnectionUrl, { useFindAndModify: false }).then(() => {
+  server = app.listen(PORT, () => {
+    logger.info(`Server is listinig on port: ${PORT}`);
+  });
+});
 
-app.use('/auth', authRouter);
+const existHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed!');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
 
-app.listen(port, () => {
-  console.log(`App listening on port: ${port}`);
+const unexpectedErrorHandler = (error: Error) => {
+  logger.error(error);
+  existHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
 });
